05a284296503a41e12cd993a96cbee9226ced90a
PalakJain0510 <40891006+PalakJain0510@users.noreply.github.com>
2018-09-13 20:10:28 +0530

    optimized validation

--- validation/main.py
+++ validation/main.py
@@ -1,7 +1,7 @@
 from validation.validationdata import ValidationData
 
 
-class CallValidation():
+class CallValidation:
     """
 
     This class call all validation methods
@@ -25,11 +25,10 @@ class CallValidation():
         :return: none
 
         """
-        self.validate_data.validate_string(pattern='alphanumeric', value="palakjain123")
-        self.validate_data.validate_email(pattern='email', value="palak.jain5@gmail.com")
-        self.validate_data.validate_date(pattern='mm-dd-yyyy', value="10-05-1994")
-        self.validate_data.date_compare(form_date="12/09/2018", to_date="09/10/2018")
-        self.validate_data.validate_numbers(pattern='card_number', value="1234-5678-1456-7895")
+        self.validate_data.validate_string(pattern="alphanumeric", value="palakjain05")
+        self.validate_data.validate_numbers(pattern='card_number', value="1234-5694-8961-4586")
+        self.validate_data.validate_date(value="10/10/2018")
+        self.validate_data.date_compare(pattern='dd-mm-yyyy', form_date="10-10-2017", to_date="28-02-2018")
 
 
 start_validation = CallValidation()
--- validation/validationdata.py
+++ validation/validationdata.py
@@ -2,8 +2,7 @@ import re
 from datetime import datetime
 
 
-
-class ValidationData():
+class ValidationData:
     """
 
     This class has all methods for validation
@@ -14,21 +13,20 @@ class ValidationData():
         """
 
         INIT of class ValidationData having dictionary which contains type of all pattern
-         'mobile_no': '(0|91)?[7-9][0-9]{9}(\d{3})\D*(\d{3})\D*(\d{4})==^(\d{3}--\d{3}--\d{4}){10}
 
         """
-        self.all_pattern = {'string': '^[a-zA-Z]+$',
+        self.all_pattern = {'string': '^[a-zA-Z]{1,255}$',
                             'alphanumeric': '^[a-zA-Z0-9]{1,255}$',
                             'specialcharacters': '^[-a-zA-Z0-9!@#$&()`.+,/\"]{1,255}$',
                             'email': '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.com)$',
                             'normal_mb': '^[0-9]{10}',
                             'mobile_no': '^(0|91)-[1-9]{3}-[0-9]{3}-[0-9]{4}$',
-                            'phone_no': '^[9,1]{2}-[1-9]{3},[0-9]{3},[0-9]{4}$',
+                            'phone_no': '^(0|91)-[1-9]{3},[0-9]{3},[0-9]{4}$',
                             'dd/mm/yyyy': '%d/%m/%Y',
                             'mm/dd/yyyy': '%m/%d/%Y',
                             'dd-mm-yyyy': '%d-%m-%Y',
                             'mm-dd-yyyy': '%m-%d-%Y',
-                            'yyyy-mm-dd':'%Y-%m-%d',
+                            'yyyy-mm-dd': '%Y-%m-%d',
                             'yyyy/mm/dd': '%Y/%m/%d',
                             'integer': '^[1-9]\d*$',
                             'whole_numbers': '^([0-9]\d*)$',
@@ -47,43 +45,47 @@ class ValidationData():
             - False -- when no pattern is specify
 
         """
-        if pattern == 'string':
-            return self.all_pattern['string']
-        if pattern == 'alphanumeric':
-            return self.all_pattern['alphanumeric']
-        elif pattern == 'specialcharacters':
-            return self.all_pattern['specialcharacters']
-        elif pattern == 'email':
-            return self.all_pattern['email']
-        elif pattern == 'normal_mb':
-            return self.all_pattern['normal_mb']
-        elif pattern == 'mobile_no':
-            return self.all_pattern['mobile_no']
-        elif pattern == 'phone_no':
-            return self.all_pattern['phone_no']
-        elif pattern == 'dd/mm/yyyy':
-            return self.all_pattern['dd/mm/yyyy']
-        elif pattern == 'mm/dd/yyyy':
-            return self.all_pattern['mm/dd/yyyy']
-        elif pattern == 'dd-mm-yyyy':
-            return self.all_pattern['dd-mm-yyyy']
-        elif pattern == 'mm-dd-yyyy':
-            return self.all_pattern['mm-dd-yyyy']
-        elif pattern == 'yyyy-mm-dd':
-            return self.all_pattern['yyyy-mm-dd']
-        elif pattern == 'yyyy/mm/dd':
-            return self.all_pattern['yyyy/mm/dd']
-        elif pattern == 'integer':
-            return self.all_pattern['integer']
-        elif pattern == 'whole_numbers':
-            return self.all_pattern['whole_numbers']
-        elif pattern == 'float':
-            return self.all_pattern['float']
-        elif pattern == 'card_number':
-            return self.all_pattern['card_number']
+        if pattern:
+            return self.all_pattern[pattern]
         else:
             print("Pattern with " + pattern + " not found")
             return False
+
+    def check_value(self, value, pattern):
+        """
+
+        This method check that input value should not be null.
+        * :param self: current instance of class
+        * :param value: input from user
+        * :type value: string
+        * :param pattern: pattern input from user
+        * :type pattern: string
+        * :return:
+            -True -- if value is not null
+            -False -- if value is null
+        * :rtype: boolean
+        """
+        if value == "":
+            print(pattern.upper() + " value is required ")
+            return False
+        else:
+            return True
+
+    def get_valid_value(self, pattern, value):
+        """
+        This method validate give valid value according to the pattern specify
+
+        * :param self: current instance of class
+        * :param pattern: specify by user
+        * :type pattern : pattern_type
+        * :param value: input value from user
+        * :type value :string
+        * :return: valid_value
+
+        """
+        pattern_type = self.get_pattern(pattern)
+        valid_value = re.match(pattern_type, value)
+        return valid_value
 
     def validate_string(self, pattern='alphanumeric', value="", min_length=1, max_length=255):
         """
@@ -106,14 +108,10 @@ class ValidationData():
 
         """
         try:
-            if value == "":
-                print(pattern + "Value is required with minimum length of " + min_length + "and with maximum length of"
-                      + max_length)
-                return False
-            else:
-                pattern_type = self.get_pattern(pattern)
-                valid_value = re.match(pattern_type, value)
-                if valid_value and len(value) <= max_length:
+            check_value = self.check_value(value, pattern)
+            if check_value is True:
+                valid_value = self.get_valid_value(pattern, value)
+                if valid_value:
                     print("validation of " + pattern + " with value " + value + " is done successfully")
                     return True
                 else:
@@ -122,16 +120,20 @@ class ValidationData():
         except Exception as e:
             print(e)
 
-    def validate_email(self, pattern='email', value=""):
+    def validate_numbers(self, pattern='card_number', value=""):
         """
 
-        This method validate email value according to the pattern specify
+        This method validate numbers values according to the pattern specify
 
         * :param self: current instance of class
-        * :param pattern: email
-        * :type email : pattern_type
+        * :param pattern: alphanumeric
+        * :type alphanumeric : pattern_type
         * :param value: input value from user
         * :type value :string
+        * :param min_length: 1
+        * :type min_length: int
+        * :param max_length: 255
+        * :type max_length: int
         * :return:
            -True -- if validation done
            -False -- if validation fails
@@ -139,12 +141,9 @@ class ValidationData():
 
         """
         try:
-            if value == "":
-                print(pattern + "Value is required")
-                return False
-            else:
-                pattern_type = self.get_pattern(pattern)
-                valid_value = re.match(pattern_type, value)
+            check_value = self.check_value(value, pattern)
+            if check_value is True:
+                valid_value = self.get_valid_value(pattern, value)
                 if valid_value:
                     print("validation of " + pattern + " with value " + value + " is done successfully")
                     return True
@@ -171,21 +170,17 @@ class ValidationData():
 
         """
         try:
-            if value == "":
-                print(pattern + "Value is required")
-                return False
-            else:
+            check_value = self.check_value(value, pattern)
+            if check_value is True:
                 pattern_type = self.get_pattern(pattern)
-                value = datetime.strptime(value, pattern_type)
-                if value:
+                date_value = datetime.strptime(value, pattern_type)
+                if date_value:
                     print("validation of " + pattern + " with value " + str(value) + " is done successfully")
                     return True
                 else:
-                    print("Enter correct value of  " + pattern)
                     return False
-
         except Exception as e:
-            print(e)
+            print(str(e) + " , please enter Correct value of " + pattern)
 
     def date_compare(self, pattern='dd/mm/yyyy', form_date="", to_date=""):
         """
@@ -205,55 +200,18 @@ class ValidationData():
         """
 
         try:
-            if form_date == "" or to_date == "":
-                print(pattern + "Value is required")
-                return False
-            else:
+            check_start_date = self.check_value(form_date, pattern)
+            check_end_date = self.check_value(to_date, pattern)
+            if check_start_date and check_end_date is True:
                 pattern_type = self.get_pattern(pattern)
                 start = datetime.strptime(form_date, pattern_type)
                 end = datetime.strptime(to_date, pattern_type)
-                if start and end:
-                    if end < start:
-                        print("end date" + str(to_date) + "can't be previous then start date" + str(form_date))
-                        return False
-                    else:
-                        print("validation of " + pattern + " with to_date " + str(to_date) + "and form_date " + str(form_date) +
-                              " is done successfully")
-                        return True
-                else:
-                    print("Enter correct value of  " + pattern)
+                if end < start:
+                    print("End date" + str(to_date) + " can't be previous then start date " + str(form_date))
                     return False
-
-        except Exception as e:
-            print(e)
-
-    def validate_numbers(self, pattern='integer', value=""):
-        """
-
-        This method validate numbers  according to the pattern specify
-
-        * :param self: current instance of class
-        * :param pattern: integer
-        * :type integer : number  pattern_type
-        * :param value: input value from user
-        * :type value :string
-        * :return:
-           -True -- if validation done
-           -False -- if validation fails
-        * :rtype : Boolean
-        """
-        try:
-            if value == "":
-                print(pattern + "Value is required")
-                return False
-            else:
-                pattern_type = self.get_pattern(pattern)
-                valid_value = re.match(pattern_type, value)
-                if valid_value:
-                    print("validation of " + pattern + " with value " + value + " is done successfully")
+                else:
+                    print("validation of " + pattern + " with form_date " + str(form_date) + "and to_date " +
+                          str(to_date) + " is done successfully ")
                     return True
-                else:
-                    print("Enter correct value of  " + pattern)
-                    return False
         except Exception as e:
-            print(e)
+            print(str(e) + " , please enter Correct value of " + pattern)
